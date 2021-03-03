705aa876dbcfb908c4ea5a79a096cbc8c866cb0a
Peter Kiraly <peter.kiraly@gwdg.de>
2021-03-04 00:15:41 +0100

    Working on isPartOf linkage.

--- scripts/isPartOf-detection.R
+++ scripts/isPartOf-detection.R
@@ -42,35 +42,36 @@ df %>%
   filter(
     !is.na(id)
     & magyar_cim %in% forrasok & id > 80
-    & grepl('Leipzig', normalized_city) & year_n == 1922
+    & grepl('Berlin', normalized_city) & year_n == 1976
   ) %>%
   select(id, szerzo, nyelv, idegen_cim, fordito, megjelenes) %>% 
   view()
 
+.id <- 70731
 df.filtered <- df %>% 
   filter(
     is.na(isPartOf)
-    & grepl('Leipzig', normalized_city) & year_n == 1922
-    & grepl('Gedichte', megjelenes, ignore.case = TRUE)
+    & grepl('Berlin', normalized_city) & year_n == 1976
+    #& grepl('Der entschlossene', megjelenes, ignore.case = TRUE)
     #& !id %in% c(163, 11800, 15159)
-    & id != 15042
-    #& nyelv == 'francia'
-    #& szerzo == 'VÖRÖSMARTY Mihály'
-    #& grepl('Унгарски Разкази', idegen_cim, ignore.case = TRUE)
+    & id != .id
+    #& nyelv == 'grúz'
+    & szerzo == 'ÖRKÉNY István'
+    #& grepl('Selbstgespraeche', idegen_cim, ignore.case = TRUE)
   ) %>% 
   view()
 
 print(count(df.filtered))
-
+.id
 df.filtered %>% 
   select(id) %>%
-  mutate(isPartOf2 = 15042) %>%
+  mutate(isPartOf2 = .id) %>%
   union(df.isPartOf) %>%
-  distinct() %>%
+  distinct() %>% 
   write_csv('data/isPartOf.csv')
 
-df.isPartOf <- read_csv('data/isPartOf.csv')
-count(df.isPartOf)
+# df.isPartOf <- read_csv('data/isPartOf.csv')
+# count(df.isPartOf)
 
 # ---------
 # mutate(megjelenes = gsub(' (стр|Стр|cтр)\\. \\d+\\.?$', '', megjelenes)) %>%
