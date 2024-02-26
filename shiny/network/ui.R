250219c96c584016714fd94c77df4bcf23161e90
Péter Király <pkiraly@gwdg.de>
2024-02-26 09:51:18 +0100

    Improve the About page

--- shiny/network/ui.R
+++ shiny/network/ui.R
@@ -10,7 +10,7 @@ navbarPage(
           h4("Connections between regions"),
           p("The network edges represent the number of authors translated in both regions."),
           sliderInput("limit",
-              label = "Minimum nr of common authors",
+              label = "Minimum number of authors",
               min = 5,
               max = 100,
               value = 10,
@@ -30,12 +30,21 @@ navbarPage(
     "About",
     fluidPage(
       wellPanel(
-        h4("What is this?"),
+        h4("Patterns of translations. Investigating the translated Hungarian literature"),
+        p("Authors: András Kiséry (CCNY, New York) and Péter Király (GWDG, Göttingen)."),
         p("This is a set of interactive statistical dashboards for the analysis of 
-          the Hungarian literature tranlated into foreign languages. The dataset
-          is based on Bibliographica Hungaricana by Tibor Demeter and 
-          Tiborné Demeter, and UNESCO's Index Translationum."),
-        p("Authors: András Kiséry (CCNY, New York) and Péter Király (GWDG, Göttingen).")
+          the Hungarian literature tranlated into foreign languages."),
+        p("The dataset is based on",
+          a("Bibliographica Hungaricana", href="http://demeter.oszk.hu", target="_blank"),
+          " by Tibor Demeter and Tiborné Demeter, and UNESCO's ", 
+          a("Index Translationum", href="https://www.unesco.org/xtrans/bsform.aspx", target="_blank"),
+          " database."),
+        p("The source code is available at ",
+          a("GiHhub", href="https://github.com/pkiraly/patterns-of-translations/", target="_blank"),
+          " while the data is available at ",
+          a("GRO.data", href="https://doi.org/10.25625/5JFAMK", target="_blank"),
+          " repository."
+        ),
       )
     )
   ),
