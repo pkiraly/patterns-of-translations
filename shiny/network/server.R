59dc75ab6c080836430bee663d56defb4baa5a8d
Péter Király <pkiraly@gwdg.de>
2025-07-11 15:49:21 +0200

    Update to the latest data

--- shiny/network/server.R
+++ shiny/network/server.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(igraph)
 library(DT)
 
-print('#### START')
 dataDir <- 'data2025'
 
 prepare_base_df <- function(df) {
@@ -38,13 +37,8 @@ edges_for_all <- function(df, common_authors_limit, minmax) {
 }
 
 getNetwork <- function(df, common_authors_limit, minmax) {
-  print('getNetwork')
   edges <- edges_for_all(df, common_authors_limit, minmax)
-  print(edges, n = Inf)
-  print('/edges_for_all')
   nodes <- extractNodes(edges)
-  print(nodes)
-  print('/extractNodes')
   graph_from_data_frame(d=edges, vertices=nodes, directed=TRUE)
 }
 
@@ -93,10 +87,8 @@ regions <- read_csv(paste0(dataDir, '/regions.csv'), show_col_types = FALSE) %>%
 
 readWorldFile <- function(world) {
   file_name <- paste0(dataDir, '/ratios-by-regions-', world, '.csv')
-  print(file_name)
   raw_df <- read_csv(file_name, show_col_types = FALSE)
   base_df <- prepare_base_df(raw_df)
-  print('readWorldFile()')
   base_df
 }
 
@@ -109,10 +101,7 @@ function(input, output, session) {
 
   readAuthorRegionPairs <- reactive({
     file_name <- paste0(dataDir, '/author-region-pairs-', input$world, '.csv')
-    print(file_name)
-    pariDF <- read_csv(file_name, show_col_types = FALSE)
-    print('readAuthorRegionPairs read')
-    return(pariDF)
+    read_csv(file_name, show_col_types = FALSE)
   })
 
   updateSelectInput(
@@ -122,17 +111,12 @@ function(input, output, session) {
   
   output$network_plot <- renderPlot({
     df <- readData()
-    print(head(df))
     if (input$country == "all" || input$country == "") {
-      print('country = all or empty')
       net <- getNetwork(df, input$limit, input$minmax)
-      print('/getNetwork()')
       shapes <- rep('none', length(V(net)))
     } else {
-      print('has a country')
       net <- getNetworkForCountry(df, input$country, input$limit, input$minmax, input$level)
       countries <- V(net)$name
-      print(countries)
       shapes <- c('none', 'circle')[as.integer(countries == input$country) + 1]
     }
     ebn <- edge_betweenness(net)
